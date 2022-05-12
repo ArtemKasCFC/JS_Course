1e893027c2e4a1cd896dbe3946595ed75e52e59e
Artem <artem.kas.cfc@gmail.com>
2022-05-12 23:45:22 +0300

    First homework

---
+++ HW1.js
@@ -0,0 +1,42 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('How much movies have you already sean?', '18');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const questionOne = prompt('One of the last watched movies?', 'Nobody'),
+    questionTwo = prompt('What is grade?', '8'),
+    questionOne_2 = prompt('One of the last watched movies?', 'Stalker'),
+    questionTwo_2 = prompt('What is grade?', '9');
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionOne_2] = questionTwo_2;
